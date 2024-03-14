d7a078319cea31249d0d52279a88cb9d9bf04cbf
Adam <simoaddame17@gmail.com>
2024-03-14 14:41:42 +0100

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(char) if char % 2 == 0 else chr(char).upper()), end="")
+    if char % 2 == 1:
+        print("{}".format(chr(char).upper()), end="")
+    else:
+        print("{}".format(chr(char)), end="")
