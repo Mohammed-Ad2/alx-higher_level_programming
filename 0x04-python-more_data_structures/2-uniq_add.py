1e41329bdeec351e303c44142d605888f5e7d7ac
Adam <simoaddame17@gmail.com>
2024-03-27 07:58:15 +0100

    update for task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     """Add all unique integers in a list (once for each integer)."""
-    uniq_list = []
-    add = 0
+    unique_integers = set()
+    total_sum = 0
     for num in my_list:
-        if num not in uniq_list:
-            add += num
-            uniq_list.append(num)
-    return add
+        if num not in unique_integers:
+            unique_integers.add(num)
+            total_sum += num
+    return total_sum
